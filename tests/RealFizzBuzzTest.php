704c9d191a4b02fde7c745fe3681be0b9de0b9db
Ernest Serunkuma <s.ernest7@gmail.com>
2021-08-01 20:58:43 -0600

    updates tests to include lucky

--- tests/RealFizzBuzzTest.php
+++ tests/RealFizzBuzzTest.php
@@ -10,9 +10,9 @@ class RealFizzBuzzTest extends TestCase {
         $input = range(1, 20);
         $output = RealFizzBuzz::step1(range(1,20));
         $this->assertEquals(
-            '1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz',
+            '1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz',
             $output,
-            "The output should match: '1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz'"
+            "The output should match: '1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz'"
         );
     }
 }
