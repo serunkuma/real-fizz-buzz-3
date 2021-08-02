e89123aa82a82bbd823b97da94794da0058f9fce
Ernest Serunkuma <s.ernest7@gmail.com>
2021-08-01 22:12:31 -0600

    completes step 3

--- src/RealFizzBuzz.php
+++ src/RealFizzBuzz.php
@@ -3,33 +3,57 @@ namespace RealFizzBuzz;
 require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 class RealFizzBuzz {
-    public static function step1($range){
+    public static $report = array(
+        'fizz' => 0,
+        'buzz' => 0,
+        'fizzbuzz' => 0,
+        'lucky' => 0,
+        'integer' => 0 
+    );
+
+    public static function step1($range) {
         $output = '';
         $rangeSize = count($range);
         
         for($i = 0; $i < $rangeSize ; $i++ ) {
             $last = $i === $rangeSize - 1 ? '' : ' ';
             $isLucky = (bool) preg_match("/3/", strval($range[$i]));
+
+            $isLucky ? self::$report['lucky']++ : null; // count lucky here and dont count it again
     
             switch ($range[$i]) {
                 case $range[$i] % 15 === 0:
                     $output .= $isLucky ? 'lucky' . $last : 'fizzbuzz' . $last;
+                    $isLucky ? null : self::$report['fizzbuzz']++;
                     break;
     
                 case $range[$i] % 5 === 0:
                     $output .= $isLucky ? 'lucky' . $last : 'buzz' . $last;
+                    $isLucky ? null : self::$report['buzz']++;
                     break;
     
                 case $range[$i] % 3 === 0:
                     $output .= $isLucky ? 'lucky' . $last : 'fizz' . $last;
+                    $isLucky ? null : self::$report['fizz']++;
                     break;
     
                 default:
                     $output .= $isLucky ? 'lucky' . $last : $range[$i] . $last;
+                    $isLucky ? null : self::$report['integer']++;
                     break;
             }
         }
         return $output;
+    }
+
+
+    public static function generateReport() {
+        return 
+        "fizz: " . self::$report['fizz'] . "\n" .
+        "buzz: " . self::$report['buzz'] . "\n" .
+        "fizzbuzz: " . self::$report['fizzbuzz'] . "\n" .
+        "lucky: " . self::$report['lucky'] . "\n" .
+        "integer: " . self::$report['integer'];
     }
 }
 
@@ -41,6 +65,7 @@ $rangeEnd = (int) empty($_GET['end']) ? null : $_GET['end'];
 <?php if($rangeStart && $rangeEnd):
     
     echo RealFizzBuzz::step1(range($rangeStart, $rangeEnd));
+    echo '<pre>' . RealFizzBuzz::generateReport() . '</pre>';
 
 else : ?>
 
--- tests/RealFizzBuzzTest.php
+++ tests/RealFizzBuzzTest.php
@@ -8,7 +8,7 @@ use RealFizzBuzz\RealFizzBuzz as RealFizzBuzz;
 class RealFizzBuzzTest extends TestCase {
     public function testStep1(){
         $input = range(1, 20);
-        $output = RealFizzBuzz::step1(range(1,20));
+        $output = RealFizzBuzz::step1($input);
         $this->assertEquals(
             '1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz',
             $output,
@@ -17,12 +17,19 @@ class RealFizzBuzzTest extends TestCase {
     }
 
     public function testReport(){
+        RealFizzBuzz::$report = array(
+            'fizz' => 0,
+            'buzz' => 0,
+            'fizzbuzz' => 0,
+            'lucky' => 0,
+            'integer' => 0 
+        );
         RealFizzBuzz::step1(range(1,20));
         $output = RealFizzBuzz::generateReport();
         $this->assertEquals(
             "fizz: 4\nbuzz: 3\nfizzbuzz: 1\nlucky: 2\ninteger: 10",
             $output,
-            "The output should match: fizz: 4\nbuzz: 3\nfizzbuzz: 1\nlucky: 2\ninteger: 10"
+            "The output should match: fizz: 4 buzz: 3 fizzbuzz: 1 lucky: 2 integer: 10"
         );
     }
 }
